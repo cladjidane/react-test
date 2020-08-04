3ed0009505a1940a12a8ee0363ae80dcfeb4c53a
Canu Fabien <fabien.canu@gmail.com>
2020-08-04 12:36:48 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ import EventListenenerRef from './components/EventListenerRef';
 function App() {
   return (
     <div className="App">
-      <h2>Event Listenener  = fail</h2>
+      <h2>Event Listener  = fail</h2>
       <EventListenenerFail />
-      <h2>Event Listenener with Var = OK</h2>
+      <h2>Event Listener with Var = OK</h2>
       <EventListenenerVar />
-      <h2>Event Listenener with Ref = OK</h2>
+      <h2>Event Listener with Ref = OK</h2>
       <EventListenenerRef />
     </div>
   );
