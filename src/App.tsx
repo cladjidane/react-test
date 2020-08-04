11c54e28d2dbde15533ee5ef7c42c7dd6c88c0e0
Canu Fabien <fabien.canu@gmail.com>
2020-08-04 10:40:13 +0200

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
 import React from 'react';
-import './App.css';
 import logo from './logo.svg';
-import { NumbersStoreProvider } from './contexts';
+import './App.css';
 
-import AddNumber from './components/AddNumber';
-import NumbersInfo from './components/NumbersInfo';
-import NumbersList from './components/NumbersList';
-
-const App: React.FC = () => {
+function App() {
   return (
-    <NumbersStoreProvider>
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} alt="MobX-state-tree logo" className="App-logo" />
-          <h2>MobX-state-tree</h2>
-        </div>
-        <AddNumber />
-        <NumbersInfo />
-        <NumbersList />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <a
-            href="https://mobx-state-tree.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn MobX-state-tree
-          </a>
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
-      </div>
-    </NumbersStoreProvider>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
